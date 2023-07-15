dc9d369747c4796ee724fb21c58a9ea9c31d1105
Abuelito <joaquinoller25@gmail.com>
2023-07-15 17:50:54 -0300

    added missing appversion.h file

---
+++ regamemod/regamedll/version/appversion.h
@@ -0,0 +1,19 @@
+#ifndef __APPVERSION_H__
+#define __APPVERSION_H__
+
+// 
+// This file is generated automatically.
+// Don't edit it.
+// 
+
+// Version defines
+#define APP_VERSION "5.22.0.0-dev"
+
+#define APP_COMMIT_DATE "Jul 15 2023"
+#define APP_COMMIT_TIME "16:40:07"
+
+#define APP_COMMIT_SHA ""
+#define APP_COMMIT_URL "/commits/"
+
+#endif //__APPVERSION_H__
+
